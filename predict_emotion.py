5d5aa4b00009fb47981c8bf544c295ceeac54404
Siddhesh Todi <siddheshtodi@gmail.com>
2025-06-24 03:16:07 +0530

    model accuracy - 22%, acheived by adding more layers and data augmentation

--- predict_emotion.py
+++ predict_emotion.py
@@ -6,7 +6,7 @@ model = load_model('emotion_model.h5')
 
 class_labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
-img = image.load_img('"E:/Siddhesh/ML/EmotionDetector/github/images/test_img2.jpg"', target_size = (48, 48), color_mode = 'grayscale')
+img = image.load_img('E:/Siddhesh/ML/EmotionDetector/github/images/surprise.jpg', target_size = (48, 48), color_mode = 'grayscale')
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array /= 255.0
--- train_model.py
+++ train_model.py
@@ -3,27 +3,27 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
-train_dir = "./train"
-test_dir = "./test"
+train_dir = "../train"
+test_dir = "../test"
 
 image_size = (48, 48)
 
 train_datagen = ImageDataGenerator(
-    rescale=1./255,               # Always normalize
-    rotation_range=30,            # Rotate images up to 30°
-    zoom_range=0.2,               # Zoom in by 20%
-    width_shift_range=0.2,        # Shift image left/right by 20%
-    height_shift_range=0.2,       # Shift image up/down by 20%
-    horizontal_flip=True          # Flip image horizontally
+    rescale=1./255,
+    rotation_range=15,
+    zoom_range=0.1,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True
 )
 
 test_datagen = ImageDataGenerator(
-    rescale=1./255,               # Always normalize
-    rotation_range=30,            # Rotate images up to 30°
-    zoom_range=0.2,               # Zoom in by 20%
-    width_shift_range=0.2,        # Shift image left/right by 20%
-    height_shift_range=0.2,       # Shift image up/down by 20%
-    horizontal_flip=True          # Flip image horizontally
+    rescale=1./255,
+    rotation_range=15,
+    zoom_range=0.1,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True
 )
 
 train_generator = train_datagen.flow_from_directory(
@@ -57,6 +57,9 @@ model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 
 model.add(Dense(128, activation='relu'))
@@ -65,19 +68,19 @@ model.add(Dropout(0.5))
 model.add(Dense(7, activation = "softmax"))
 
 model.compile(
-    loss='categorical_crossentropy',  # Good for multi-class classification
-    optimizer='adam',                 # Fast and reliable optimizer
-    metrics=['accuracy']              # To track performance
+    loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
 )
 
 loss, accuracy = model.evaluate(test_generator)
-print("Test accuracy:", accuracy)
-
+print("Test Loss:", loss)
+print("Test Accuracy:", accuracy)
 
 history = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // train_generator.batch_size,
-    epochs=20,
+    epochs=40,
     validation_data=test_generator,
     validation_steps=test_generator.samples // test_generator.batch_size
 )
